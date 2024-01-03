844b430ce6de351be8aaa9f99039e495aa5e8d90
ozioma45 <oziomaegole@gmail.com>
2024-01-03 19:10:46 +0100

    State in React

--- Code_stoic/reactpro/src/App.jsx
+++ Code_stoic/reactpro/src/App.jsx
@@ -1,10 +1,11 @@
 import ConditionalComponent from "./components/ConditionalComponent";
+import Counter from "./components/Counter";
 import Fruits from "./components/Fruits";
 
 function App() {
   return (
     <div className="App">
-      <Fruits />
+      <Counter />
     </div>
   );
 }
