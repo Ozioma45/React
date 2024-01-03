357af3d58919bce1a4711cb7af8ee86d3db87b3f
ozioma45 <oziomaegole@gmail.com>
2024-01-03 10:32:04 +0100

    conditionally render component

--- Code_stoic/reactpro/src/components/ConditionalComponent.jsx
+++ Code_stoic/reactpro/src/components/ConditionalComponent.jsx
@@ -1,16 +1,11 @@
+import Code from "./Code";
+import Welcome from "./Welcome";
+
 export default function ConditionalComponent() {
   const display = false;
   if (display) {
-    return (
-      <div>
-        <h3>This is a conditional component</h3>
-      </div>
-    );
+    return <Welcome />;
   } else {
-    return (
-      <div>
-        <h3>code everyday!</h3>
-      </div>
-    );
+    return <Code />;
   }
 }
