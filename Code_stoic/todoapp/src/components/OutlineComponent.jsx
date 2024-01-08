d66ee1b35df1865b1c1bf227544d2f9cb98cc850
ozioma45 <oziomaegole@gmail.com>
2024-01-09 00:07:17 +0100

    css module

---
+++ Code_stoic/todoapp/src/components/OutlineComponent.jsx
@@ -0,0 +1,8 @@
+import styles from "./OutlineComponent.module.css";
+export default function OutlineComponent() {
+  return (
+    <div>
+      <h1 className={styles.header}>This is an Outline Component</h1>
+    </div>
+  );
+}
