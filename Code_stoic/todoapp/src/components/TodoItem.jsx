3b6949a491ef6aee0d6335c61fcc389e32dedb63
ozioma45 <oziomaegole@gmail.com>
2024-01-09 03:37:03 +0100

    adding a delete button

--- Code_stoic/todoapp/src/components/TodoItem.jsx
+++ Code_stoic/todoapp/src/components/TodoItem.jsx
@@ -3,7 +3,13 @@ import styles from "./TodoItem.module.css";
 export default function TodoItem({ item }) {
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}{" "}
+        <span>
+          <button className={styles.deleteBtn}>X</button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
